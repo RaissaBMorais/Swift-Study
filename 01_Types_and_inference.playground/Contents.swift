9e7c0402580bb85dbb0941222e672c807c821417
Raissa Morais <raissa.xmorais@gmail.com>
2018-11-21 15:34:09 -0300

    Add types and inference study

---
+++ 01_Types_and_inference.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// Declaring a String
+var name: String = "First Last"
+print(name)
+
+// Declaring an Int
+var age: Int = 26
+
+// Float vs. Double
+// Double has twice the accuracy and is preferable over Float
+var latitude: Double = 36.166667
+
+// Number 5 is cut from the end due to accuracy
+var longitude: Float = -86.783335
+
+// Boolean
+var isHouseClean: Bool = true
+
+// Type inference
+
+// String
+var newName = "New Name"
+// Int
+var newAge = 27
+// Double
+var lat = -82.3332
